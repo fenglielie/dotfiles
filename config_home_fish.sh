121ceffed45563926398d0325741399b22d6639f
fenglielie <fenglielie@qq.com>
2024-07-19 01:50:08 +0800

    update fish config

--- config_home_fish.sh
+++ config_home_fish.sh
@@ -4,13 +4,16 @@
 script_root_dir=$(dirname "$(realpath "$0")")
 
 # Define target directory
-target_dir="$HOME/.config/fish"
+target_dir="$HOME/.config/fish/conf.d"
+target_file="$target_dir/mysetup.fish"
 
-# Check if target directory already exists
-if [ -e "$target_dir" ]; then
-    echo "Error: Directory '$target_dir' already exists. Please remove it or choose another location."
+# Ensure the target directory exists
+mkdir -p "$target_dir"
+
+if [ -e "$target_file" ]; then
+    echo "Error: File '$target_file' already exists. Please remove it or choose another location."
 else
     # Create symlink
-    ln -sf "$script_root_dir/fish" "$target_dir"
+    ln -sf "$script_root_dir/fish/conf.d/mysetup.fish" "$target_file"
     echo "Fish config finished."
 fi
