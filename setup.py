0279a53686d57ede4cd76aaf6e2ede2f36656988
fenglielie <fenglielie@qq.com>
2024-09-08 00:16:31 +0800

    style: update setup.py (use logging)

--- setup.py
+++ setup.py
@@ -3,28 +3,18 @@
 import os
 import json
 import platform
+import logging
 
-# ANSI color codes
-RESET = "\033[0m"
-INFO_COLOR = "\033[94m"
-ERROR_COLOR = "\033[91m"
-
-
-def print_info(message):
-    print(f"{INFO_COLOR}[INFO]{RESET} {message}")
-
-
-def print_error(message):
-    print(f"{ERROR_COLOR}[ERROR]{RESET} {message}")
+# Configure logging to output to the console
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 
 
 def create_symlink(src, dest):
     if os.path.exists(dest):
-        print_error(
+        logging.error(
             f"File or directory '{dest}' already exists. Please remove it and try again."
         )
     else:
-        # print_info(f"Preparing to create symlink from '{src}' to '{dest}'.")
         user_input = input("Do you want to proceed? (y/n): ").strip().lower()
         if user_input == "y":
             try:
@@ -32,11 +22,11 @@ def create_symlink(src, dest):
                     os.symlink(src, dest, target_is_directory=True)
                 else:
                     os.symlink(src, dest)
-                print_info("Symlink created successfully.")
+                logging.info("Symlink created successfully.")
             except Exception as e:
-                print_error(f"Failed to create symlink': {e}")
+                logging.error(f"Failed to create symlink: {e}")
         else:
-            print_info("Operation skipped by user.")
+            logging.info("Operation skipped by user.")
 
 
 def expand_path(path):
@@ -59,46 +49,48 @@ def handle_config(script_root_dir, destination_root, entries):
         dest = expand_path(os.path.join(destination_root, entry["destination"]))
 
         dest_dir = os.path.dirname(dest)
-        print_info(f"Source: '{src}'")
-        print_info(f"Dest:   '{dest}'")
+        logging.info(f"Source: '{src}'")
+        logging.info(f"Dest:   '{dest}'")
         if not os.path.exists(dest_dir):
-            print_info(f"Directory '{dest_dir}' does not exist. Creating it now.")
+            logging.info(f"Directory '{dest_dir}' does not exist. Creating it now.")
             os.makedirs(dest_dir)
-            print_info(f"Created directory: {dest_dir}")
+            logging.info(f"Created directory: {dest_dir}")
 
         create_symlink(src, dest)
 
 
 def main():
-    print_info("dotfiles setup started.")
+    logging.info("dotfiles setup started.")
 
     # Get root dir
     script_root_dir = os.path.dirname(os.path.realpath(__file__))
-    print_info(f"Script root directory: {script_root_dir}")
+    logging.info(f"Script root directory: {script_root_dir}")
 
     # Load configuration from JSON file
     config_file_path = os.path.join(script_root_dir, "config.json")
-    print_info(f"Loading configuration from '{config_file_path}'")
+    logging.info(f"Loading configuration from '{config_file_path}'")
 
     if not os.path.exists(config_file_path):
-        print_error(f"Configuration file '{config_file_path}' not found.")
+        logging.error(f"Configuration file '{config_file_path}' not found.")
         return
 
     with open(config_file_path, "r") as config_file:
         config = json.load(config_file)
 
     system_platform = platform.system().lower()
-    print_info(f"Detected platform: {system_platform}")
+    logging.info(f"Detected platform: {system_platform}")
 
     for group in config["groups"]:
         if system_platform in group["destination_root"]:
             destination_root = expand_path(group["destination_root"][system_platform])
-            print_info(f"Processing group with destination root: {destination_root}")
+            logging.info(f"Processing group with destination root: {destination_root}")
             handle_config(script_root_dir, destination_root, group["entries"])
         else:
-            print_info(f"Skipping group due to unsupported platform: {system_platform}")
+            logging.info(
+                f"Skipping group due to unsupported platform: {system_platform}"
+            )
 
-    print_info("dotfiles setup completed.")
+    logging.info("dotfiles setup completed.")
 
 
 if __name__ == "__main__":
