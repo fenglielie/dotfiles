f2d6d4e6dd84eeb8883eb7a1f64ab6a7b03cdadd
fenglielie <fenglielie@qq.com>
2024-11-16 20:10:27 +0800

    rename bash/config.d -> bash/func

--- bash/init_bash.sh
+++ bash/init_bash.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-for script in "$SCRIPT_DIR"/config.d/*.sh; do
+for script in "$SCRIPT_DIR"/func/*.sh; do
     if [ -f "$script" ]; then
         # echo "source $script"
         source "$script"
