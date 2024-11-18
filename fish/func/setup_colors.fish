057c8026585c7451930551a48f1658ff0eb76d13
fenglielie <fenglielie@qq.com>
2024-11-18 21:36:06 +0800

    feat: omp auto update ?

--- fish/func/setup_colors.fish
+++ fish/func/setup_colors.fish
@@ -1,7 +1,5 @@
 # echo "call setup"
 
-set -U __fish_initialized 3100
-
 set -U fish_color_autosuggestion 707A8C
 set -U fish_color_cancel \x2dr
 set -U fish_color_command 5CCFE6
@@ -26,12 +24,7 @@ set -U fish_color_status red
 set -U fish_color_user brgreen
 set -U fish_color_valid_path \x2d\x2dunderline
 
-set -U fish_key_bindings fish_default_key_bindings
-
 set -U fish_pager_color_completion normal
 set -U fish_pager_color_description B3A06D\x1eyellow
 set -U fish_pager_color_prefix normal\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
 set -U fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
-
-# close default greeting message
-# set -U fish_greeting
