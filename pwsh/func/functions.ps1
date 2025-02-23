bc8bae4ccf094ac2ea8663d2a6e2e0d9e01ab97a
fenglielie <fenglielie@qq.com>
2025-02-23 21:22:35 +0800

    feat: simplify pwsh/func/functions

--- pwsh/func/functions.ps1
+++ pwsh/func/functions.ps1
@@ -1,7 +1,7 @@
 ##################################################################
 # cd ...
 
-function Set-SimpleCd {
+function Set-MyCd {
     param
     (
         $Path = $null, # 要更改到的目标目录路径，默认为空
@@ -28,7 +28,7 @@ function Set-SimpleCd {
 ##################################################################
 # which
 
-function Get-SimpleWhich {
+function Get-MyWhich {
     param (
         [string]$CommandName
     )
@@ -46,7 +46,7 @@ function Get-SimpleWhich {
 ##################################################################
 # touch
 
-function New-SimpleTouch {
+function New-MyTouch {
     param (
         [parameter(Mandatory = $true, Position = 0)]
         [string]$FilePath
@@ -65,7 +65,7 @@ function New-SimpleTouch {
 ##################################################################
 # mkdir
 
-function New-SimpleMkdir {
+function New-MyMkdir {
     param (
         [parameter(Mandatory = $true, Position = 0)]
         [string]$directoryPath
@@ -79,131 +79,10 @@ function New-SimpleMkdir {
 }
 
 ##################################################################
-# ls
-
-function Get-SimpleLs {
-    param (
-        [string]$Path = ".",
-        [switch]$l,  # Long listing format
-        [switch]$a,  # Show all files, including hidden
-        [switch]$F   # Append file type indicator
-    )
-
-    # 获取指定路径下的文件和文件夹
-    $items = Get-ChildItem -Force $Path
-
-    # 处理 -a 选项
-    if (-not $a) {
-        $items = $items | Where-Object { $_.Name -notlike '.*' }
-    }
-
-    # 如果没有文件，直接返回
-    if ($items.Count -eq 0) {
-        return
-    }
-
-    if ($l) {
-        $items | Format-Table -AutoSize Mode, Length, LastWriteTime, Name
-    } else {
-        $output = @()
-        $colors = @()
-        foreach ($item in $items) {
-            $name = $item.Name
-            $color = "White"
-
-            # 添加文件类型指示符和颜色
-            if ($item.PSIsContainer) {
-                $typeIndicator = "/"
-                $color = "Blue"  # 目录为蓝色
-            } elseif ($item.Attributes -match "Executable") {
-                $typeIndicator = "*"
-                $color = "Green"  # 可执行文件为绿色
-            } elseif ($item.Extension -eq ".lnk") {
-                $typeIndicator = "@"
-                $color = "Yellow"  # 链接文件为黄色
-            }
-            else{
-                $typeIndicator = ""
-                $color = "White"  # 其他文件为白色
-            }
-
-            $name = $name + $typeIndicator
-
-            $output += $name
-            $colors += $color
-        }
-
-        for ($i = 0; $i -lt $output.Count; $i++) {
-            $item = $output[$i] + " "
-            if (($i + 1) % 4 -eq 0) {
-                Write-Host $item -ForegroundColor $colors[$i]
-            }
-            else{
-            Write-Host $item -ForegroundColor $colors[$i] -NoNewline
-            }
-        }
-    }
-}
-
-
-function Get-SimpleLl {
-    param (
-        [string]$Path = "."
-    )
-    Get-SimpleLs -Path $Path -a -l -F
-}
-
-function Get-SimpleLa {
-    param (
-        [string]$Path = "."
-    )
-    Get-SimpleLs -Path $Path -A
-}
-
-function Get-SimpleL {
-    param (
-        [string]$Path = "."
-    )
-    Get-SimpleLs -Path $Path -F
-}
-
-##################################################################
-# vi/vim -> notepad in windows
-
-function Open-SimpleNotepad {
-    param (
-        [string]$FilePath
-    )
-
-    # 如果提供了文件路径参数
-    if ($FilePath) {
-        # 检查文件是否存在
-        if (-not (Test-Path $FilePath)) {
-            # 文件不存在时，检查提供的参数是否为相对路径
-            if (-not [System.IO.Path]::IsPathRooted($FilePath)) {
-                # 如果是相对路径，则加上当前位置得到绝对路径
-                $FilePath = (Join-Path (Get-Location).Path $FilePath)
-            }
-            Write-Host "Create new file: $FilePath"
-
-            notepad /q $FilePath
-        }
-        else {
-            # 文件存在
-            notepad $FilePath
-        }
-    }
-    else {
-        # 如果未提供文件路径参数，则直接打开
-        notepad
-    }
-}
-
-##################################################################
 
 
 # 判断别名是否已经存在，如果存在则不替换，这个工具函数不导出
-function Add-SimpleAliasIfNoExists {
+function Add-MyAliasIfNoExists {
     param (
         [string]$AliasName,
         [string]$AliasToName
@@ -228,68 +107,26 @@ function Add-SimpleAliasIfNoExists {
     }
 }
 
-function Get-SimplePwd {
+function Get-MyPwd {
     (Get-Location).ToString()
 }
 
-function Get-SimplePath{
+function Get-MyPath{
     $env:PATH -split ';'
 }
-
-# 检查路径是否在全局 PATH 中
-function Test-SimplePath{
-    param (
-        [string]$Directory = $PWD.Path
-    )
-    return $env:PATH -split ';' -contains $Directory
-}
-
-function Add-SimpleTempPath{
-    param (
-        [string]$Directory = $PWD.Path
-    )
-
-    # 检查目录是否存在
-    if (-not (Test-Path $Directory)) {
-        Write-Error "Directory '$Directory' does not exist."
-        return
-    }
-
-    # 获取当前 PATH 环境变量值
-    $currentPath = $env:PATH
-
-    # 检查路径是否已经存在于 PATH 中
-    if ($currentPath -split ';' -contains $Directory) {
-        Write-Output "Directory '$Directory' is already in PATH."
-        return
-    }
-
-    # 将路径添加到 PATH 中
-    $env:PATH = "$currentPath;$Directory"
-    Write-Output "Directory '$Directory' added to PATH temporarily."
-}
-
-
 
 ##################################################################
 
 # 这些命令如果不存在，则通过powershell函数伪装实现
-Add-SimpleAliasIfNoExists vi Open-SimpleNotepad
-Add-SimpleAliasIfNoExists vim Open-SimpleNotepad
-Add-SimpleAliasIfNoExists which Get-SimpleWhich
-Add-SimpleAliasIfNoExists touch New-SimpleTouch
+
+Add-MyAliasIfNoExists which Get-MyWhich
+Add-MyAliasIfNoExists touch New-MyTouch
 
 # 别名
-Set-Alias -Name cd -Value Set-SimpleCd -Force -Option "AllScope"
-Set-Alias -Name ls -Value Get-SimpleLs
-Set-Alias -Name la -Value Get-SimpleLa
-Set-Alias -Name ll -Value Get-SimpleLl
-Set-Alias -Name l -Value Get-SimpleL
-Set-Alias -Name pwd -Value Get-SimplePwd
-Set-Alias -Name mkdir -Value New-SimpleMkdir
-Set-Alias -Name path -Value Get-SimplePath
-Set-Alias -Name pathcheck -Value Test-SimplePath
-Set-Alias -Name pathadd -Value Add-SimpleTempPath
+Set-Alias -Name cd -Value Set-MyCd -Force -Option "AllScope"
+Set-Alias -Name pwd -Value Get-MyPwd
+Set-Alias -Name mkdir -Value New-MyMkdir
+Set-Alias -Name path -Value Get-MyPath
 
 ##################################################################
 
@@ -298,9 +135,3 @@ Set-PSReadLineOption -Colors @{
     Command              = "blue"
     Parameter            = "green"
 }
-# PSReadLineOption的颜色只影响输入时的当前行渲染，不会影响输出，输出的颜色受到terminal主题的影响
-# PSReadLineOption的设置：
-# 如果使用文字，则具体颜色基于terminal的主题；
-# 如果直接使用RGB，则不受terminal主题的影响
-
-Set-Alias -Name cd -Value Set-SimpleCd -Force -Option "AllScope"
