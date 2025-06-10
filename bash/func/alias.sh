ea632be453ee7fac9b3d406247360a21c2da020e
fenglielie <fenglielie@qq.com>
2025-06-10 16:06:49 +0800

    update ls alias

--- bash/func/alias.sh
+++ bash/func/alias.sh
@@ -1,6 +1,7 @@
 # echo "call alias"
 
-alias ll='ls -alF'
+alias ll='ls -lFh'
+alias lla='ls -AlFh'
 alias la='ls -A'
 alias l='ls -CF'
 alias rm='rm -i'
--- fish/func/alias.fish
+++ fish/func/alias.fish
@@ -1,6 +1,7 @@
 # echo "call alias"
 
-abbr -a ll ls -alF
+abbr -a ll ls -lFh
+abbr -a lla ls -AlFh
 abbr -a la ls -A
 abbr -a l ls -CF
 abbr -a rm rm -i
